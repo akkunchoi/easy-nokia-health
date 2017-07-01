0ea92d36374eb2025e450fe45a26fb47186ebf68
Akiyoshi Tanaka <akkunchoi@gmail.com>
2017-07-01 14:45:23 +0900

    refactor: use async/await promisify

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const exec = require('child_process').exec;
 
+/**
+ * @name WithingsAccessToken
+ * @property {string} token
+ * @property {string} secret
+ * @property {string} userid
+ */
+/**
+ * 
+ */
 class WithingsClient {
   
   constructor(config) {
@@ -24,85 +33,86 @@ class WithingsClient {
   
   async prepare() {
     
-    const accessToken = await this.saveAccessTokenIfNotExist();
+    return await this.loadAccessToken();
 
-    return new Promise((resolve, reject) => {
-      
-      var options = {
-        consumerKey: this.config.CONSUMER_KEY,
-        consumerSecret: this.config.CONSUMER_SECRET,
-        accessToken: accessToken.token,
-        accessTokenSecret: accessToken.secret,
-        userID: accessToken.userid
-      };
-      var client = new Withings(options);
-
-      client.getDailySteps(new Date(), function(err, data) {
-        console.log(err, data);
-      });
-      
-    })
-    
   }
   
-  saveAccessTokenIfNotExist() {
-    
-    return Promise.resolve()
-      
-      .then(() => {
-        return require(this.config.CREDENTIAL_PATH)
-      })
-    
-      .catch(() => {
-      
-        return Promise.resolve()
-          
-          .then(() => {
-            return mkdirp(path.dirname(this.config.CREDENTIAL_PATH));
-          })
-        
-          .then(() => {
-            return this.getRequestToken().then((token) => {
-
-              console.log('Go ' + token.authorizeUrl);
-              let cmd = 'open -a "Google Chrome" "' + token.authorizeUrl + '"';
-              exec(cmd);
-
-              var app = express();
-              app.use(cookieParser());
-              app.listen(this.config.SERVER_PORT);
-
-              return new Promise((resolve, reject) => {
-                app.get('/oauth_callback', (req, res) => {
-                  var verifier = req.query.oauth_verifier;
-                  var options = {
-                    consumerKey: this.config.CONSUMER_KEY,
-                    consumerSecret: this.config.CONSUMER_SECRET,
-                    callbackUrl: this.config.CALLBACK_URL,
-                    userID: req.query.userid
-                  };
-                  var client = new Withings(options);
-
-                  // Request an access token
-                  client.getAccessToken(token.token, token.secret, verifier, (err, token, secret) => {
-                    if (err) return reject(err);
-                    resolve({
-                      token: token,
-                      secret: secret,
-                      userid: req.query.userid
-                    })
-                  });
-
-                });
-              });
-
-            });
-          })
-          .then((accessToken) => {
-            return writeFile(this.config.CREDENTIAL_PATH, JSON.stringify(accessToken));
-          })
-      });
+  async getCredential() {
+    return require(this.config.CREDENTIAL_PATH);
   }
+
+  /**
+   * コンシューマーキーから認証URLを生成、ブラウザを開き、コールバックを受け取ったらアクセストークンを返す
+   * 
+   * @return {Promise.<WithingsAccessToken>}
+   */
+  async getAccessToken() {
+    const token = this.getRequestToken();
+    
+    console.log('Go ' + token.authorizeUrl);
+    let cmd = 'open -a "Google Chrome" "' + token.authorizeUrl + '"';
+    exec(cmd);
+
+    var app = express();
+    app.use(cookieParser());
+    app.listen(this.config.SERVER_PORT);
+
+    return new Promise((resolve, reject) => {
+      app.get('/oauth_callback', (req, res) => {
+        var verifier = req.query.oauth_verifier;
+        var options = {
+          consumerKey: this.config.CONSUMER_KEY,
+          consumerSecret: this.config.CONSUMER_SECRET,
+          callbackUrl: this.config.CALLBACK_URL,
+          userID: req.query.userid
+        };
+        var client = new Withings(options);
+
+        // Request an access token
+        client.getAccessToken(token.token, token.secret, verifier, (err, token, secret) => {
+          if (err) return reject(err);
+          resolve({
+            token: token,
+            secret: secret,
+            userid: req.query.userid
+          })
+        });
+
+      });
+    });
+  }
+
+  /**
+   * accessTokenを取得して書き込みする
+   * 
+   * @return {Promise.<void>}
+   */
+  async saveCredential() {
+    await mkdirp(path.dirname(this.config.CREDENTIAL_PATH));
+    const token = await this.getAccessToken();
+    await writeFile(this.config.CREDENTIAL_PATH, JSON.stringify(token));
+  }
+
+  /**
+   * accessTokenがなければ取得しにいく
+   * 
+   * @return {Promise.<WithingsAccessToken>}
+   */
+  loadAccessToken() {
+
+    return this.getCredential()
+      .catch(() => {
+        return this.saveCredential();
+      })
+      .then((token) => {
+        this.accessToken = token;
+      });
+    
+  }
+
+  /**
+   * @return {Promise.<{token: string, secret: string, authorizeUrl: string}>}
+   */
   getRequestToken() {
     var options = {
       consumerKey: this.config.CONSUMER_KEY,
@@ -122,13 +132,46 @@ class WithingsClient {
       });
     })
   }
+
+  /**
+   * 
+   * @return {WithingsClient}
+   */
+  getWithingsClient() {
+    var options = {
+      consumerKey: this.config.CONSUMER_KEY,
+      consumerSecret: this.config.CONSUMER_SECRET,
+      accessToken: this.accessToken.token,
+      accessTokenSecret: this.accessToken.secret,
+      userID: this.accessToken.userid
+    };
+    var willPromise = [
+      'getDailySteps',
+      'getDailyCalories',
+      'getWeightMeasures',
+      'getPulseMeasures',
+      'getSleepSummary'
+    ]
+    if (!this.withingsClient) {
+      this.withingsClient = new Withings(options);
+      for (let key of willPromise) {
+        this.withingsClient[key] = Promise.promisify(this.withingsClient[key].bind(this.withingsClient));
+      }
+    }
+    return this.withingsClient;
+  }
+  getStep(date) {
+    return this.getWithingsClient().getDailySteps(date);
+  }
   
+  async run() {
+    await this.prepare();
+    console.log(await this.getStep(new Date()));
+  }
 }
 
 const w = new WithingsClient(config);
-w.prepare().then((res) => {
-  console.log(res);
-})
-  .catch((err) => {
-    console.error(err, err.stack);
-  })
+
+w.run().catch((err) => {
+  console.error(err, err.stack);
+});
