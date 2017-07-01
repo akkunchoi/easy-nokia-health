48073a38d2f469e9a097d98d89e734901926c109
Akiyoshi Tanaka <akkunchoi@gmail.com>
2017-07-01 12:47:23 +0900

    Added withings api access

---
+++ index.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const env = require('node-env-file');
+const config = env(__dirname + '/.env');
+
+var express = require('express')
+var app = express()
+var Withings = require('withings-lib');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+app.use(cookieParser());
+app.use(session({secret: 'bigSecret'}));
+app.listen(config.SERVER_PORT);
+
+// OAuth flow
+app.use((req, res, next) => {
+  console.log('express', req.url);
+  next();
+});
+app.get('/', function (req, res) {
+    // Create an API client and start authentication via OAuth
+    var options = {
+        consumerKey: config.CONSUMER_KEY,
+        consumerSecret: config.CONSUMER_SECRET,
+        callbackUrl: config.CALLBACK_URL
+    };
+    var client = new Withings(options);
+
+    client.getRequestToken(function (err, token, tokenSecret) {
+        if (err) {
+            // Throw error
+            return;
+        }
+
+        req.session.oauth = {
+            requestToken: token,
+            requestTokenSecret: tokenSecret
+        };
+        
+        res.redirect(client.authorizeUrl(token, tokenSecret));
+    });
+});
+
+// On return from the authorization
+app.get('/oauth_callback', function (req, res) {
+    var verifier = req.query.oauth_verifier
+    var oauthSettings = req.session.oauth
+    var options = {
+        consumerKey: config.CONSUMER_KEY,
+        consumerSecret: config.CONSUMER_SECRET,
+        callbackUrl: config.CALLBACK_URL,
+        userID: req.query.userid
+    };
+    var client = new Withings(options);
+
+    // Request an access token
+    client.getAccessToken(oauthSettings.requestToken, oauthSettings.requestTokenSecret, verifier,
+        function (err, token, secret) {
+            if (err) {
+                // Throw error
+                return;
+            }
+
+            oauthSettings.accessToken = token;
+            oauthSettings.accessTokenSecret = secret;
+
+            res.redirect('/activity/steps?userid=' + req.query.userid);
+        }
+    );
+});
+
+// Display today's steps for a user
+app.get('/activity/steps', function (req, res) {
+    var options = {
+        consumerKey: config.CONSUMER_KEY,
+        consumerSecret: config.CONSUMER_SECRET,
+        accessToken: req.session.oauth.accessToken,
+        accessTokenSecret: req.session.oauth.accessTokenSecret,
+        userID: req.query.userid
+    };
+    var client = new Withings(options);
+
+    client.getDailySteps(new Date(), function(err, data) {
+        if (err) {
+            res.send(err);    
+        }
+        res.json(data);
+    });
+});
+
